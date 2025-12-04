dadc81de897b3b5ee262abeda817f8d290ad9d07
yj <693122713@qq.com>
2025-12-04 19:10:45 +0800

    更新 payer 字段为signer

--- tests/BlockExprole/GetBlockbyHeight.py
+++ tests/BlockExprole/GetBlockbyHeight.py
@@ -14,7 +14,7 @@ class GetBlockbyHeight(BaseData):
         getblockheight=GetLatestBlockHeight()
         heightdata=getblockheight.test_getLatestBlockHeight()
 
-        url = self.base_url + "/demo/block/by-height?height="+str(heightdata)
+        url = self.base_url + "/demo/block/by-height?height="+str(int(heightdata)-1)
         print("请求地址为" + url)
         # 发送 GET 请求
         response = requests.get(url, headers=self.headers)
