065ea4b6423256b756fb894232252402b0111fe9
yj <693122713@qq.com>
2025-12-30 17:18:03 +0800

    新增行情服务接口

--- src/core/util/lark.py
+++ src/core/util/lark.py
@@ -3,14 +3,22 @@ import json
 import requests
 
 
-def sendlark(msg,robotrul,resulturl,Uid):
+def sendlark(msg,robotrul,resulturl,uid=None):
     # 发送POST请求
-    payload = {
-        "msg_type": "text",  # 消息类型：文本
-        "content": {
-            "text": msg+"接口报错了,测试报告地址为："+resulturl+"<at user_id='ou_503afccc8a9a7b0c51c02db96ec37099'>Tom</at>"
+    if uid!=None:
+        payload = {
+            "msg_type": "text",  # 消息类型：文本
+            "content": {
+                "text": msg + "接口报错了,测试报告地址为：" + resulturl + "<at user_id=" + uid + ">负责人</at>"
+            }
         }
-    }
+    else:
+        payload = {
+            "msg_type": "text",  # 消息类型：文本
+            "content": {
+                "text": msg + "接口报错了,测试报告地址为：" + resulturl
+            }
+        }
     response = requests.post(
         url=robotrul,
         headers={"Content-Type": "application/json"},  # 必须指定JSON格式
